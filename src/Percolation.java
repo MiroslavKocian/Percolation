bb0c619743b7e284ee317176b4e0d70b63da5834
MiroslavKocian <mirkocian@gmail.com>
2015-02-26 15:50:26 +0100

    add the ability to check if a site is full by isFull() method

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Percolation {
 
@@ -17,6 +20,29 @@ public class Percolation {
 
     public boolean isOpen(int row, int column) {
         return grid[row][column];
+    }
+    
+    public boolean isFull(int row, int column) { 
+        return isFull(row, column, new ArrayList<String>()); 
+    }
+
+    private boolean isFull(int row, int column, List<String> positionsChecked) {
+        if (!isOpen(row, column)) return false;
+        if (row == 0) return true;
+        
+        return checkIfNeighborsFull(row, column, positionsChecked);
+    }
+
+    private boolean checkIfNeighborsFull(int row, int column, List<String> positionsChecked) {
+        String currentPosition = String.format("(%s, %s)", row, column);
+        
+        if (positionsChecked.contains(currentPosition)) return false;
+        positionsChecked.add(currentPosition);
+        
+        return isFull(row - 1, column, positionsChecked) ||
+               isFull(row + 1, column, positionsChecked) ||
+               column > 0 && isFull(row, column - 1, positionsChecked) ||
+               column < grid.length && isFull(row, column + 1, positionsChecked);
     }
 
 }
--- test/PercolationTest.java
+++ test/PercolationTest.java
@@ -44,6 +44,78 @@ public class PercolationTest extends TestCase {
         assertFalse(percolation.isOpen(0, 0));
     }
     
+    public void testOpenSitesOnTheTopRowAreFull() {
+        assertFalse(percolation.isFull(0, 2));
+        assertFalse(percolation.isFull(2, 3));
+        
+        percolation.open(0, 2);
+        percolation.open(2, 3);
+        
+        assertTrue(percolation.isFull(0, 2));
+        assertFalse(percolation.isFull(2, 3));
+    }
     
+    public void testSitesThatAreSouthOfAFullSiteAreFull() {
+        percolation.open(0, 1);
+        
+        for(int row = 1; row < percolation.getGrid().length; row++) {
+            assertFalse(percolation.isFull(row, 1));
+            
+            percolation.open(row, 1);
+            
+            assertTrue(percolation.isFull(row, 1));
+        }
+    }
+    
+    public void testOpenSitesToTheEastOfTheFullSitesAreFull() {
+        percolation.open(0,0);
+        for (int column = 0; column < percolation.getGrid().length; column++) {
+            assertFalse(percolation.isFull(1, column));
+            percolation.open(1, column);
+            assertTrue(String.format("Site (%s, %s) should be full", 1, column), percolation.isFull(1, column));
+        }
+    }
+    
+    public void testOpenSitesDiagonalToFullSitesAreNotFull() {
+        percolation.open(0, 0);
+        percolation.open(1, 1);
+        
+        assertFalse(percolation.isFull(1, 1));
+    }
+    
+    public void testChecksSouthForFullness() {
+        percolation.open(1, 1);
+        percolation.open(2, 1);
+        percolation.open(2, 2);
+        percolation.open(2, 3);
+        percolation.open(1, 3);
+        percolation.open(0, 3);
+        
+        assertTrue(percolation.isFull(1,1));
+    }
+    
+    public void testOpenSitesToTheWestOfTheFullSitesAreFull() {
+        assertOpenSitesToTheWestOfFullSitesAreFull(percolation);
+    }
+    
+    public void testOpenSitesToTheWestOfFullSitesAreFullRegardlessOfGridSize() {
+        Percolation percolation = new Percolation(6);
+        
+        assertOpenSitesToTheWestOfFullSitesAreFull(percolation);
+    }
 
+    private void assertOpenSitesToTheWestOfFullSitesAreFull(Percolation percolation) {
+        int lastColumn = percolation.getGrid().length - 1;
+        percolation.open(0, lastColumn);
+        
+        for (int column = lastColumn; column >= 0; column--) {
+            assertFalse(percolation.isFull(1, column));
+            percolation.open(1, column);
+            assertTrue(percolation.isFull(1, column));
+        }
+    }
+    
+    
+    
+    
 }
